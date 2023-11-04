795fca40e41dc361bbcfe771f2673feb455d5b55
dalitacaridad <118063690+dalitacaridad@users.noreply.github.com>
2023-11-04 17:12:47 +0100

    algunos cambios
    funciona mejor

--- JS/contacto.js
+++ JS/contacto.js
@@ -2,48 +2,51 @@ const formulario = document.getElementById("formulario");
 const nombre = document.getElementById("nombre");
 const email = document.getElementById("email");
 const mensaje = document.getElementById("mensaje");
-const errorNombre = document.getElementById("error-nombre");
-const errorEmail = document.getElementById("error-email");
-const errorMensaje = document.getElementById("error-mensaje");
+const modal = document.getElementById("agradecimientoModal");
+const closeModal = document.getElementById("closeModal");
 
 formulario.addEventListener("submit", function (e) {
+    e.preventDefault(); 
+
+    if (formularioValido()) {
+
+        modal.style.display = "block";
+    }
+});
+
+function formularioValido() {
     let valid = true;
 
+
     if (nombre.value.trim() === "") {
-        errorNombre.textContent = "Por favor, ingrese su nombre.";
         valid = false;
-    } else {
-        errorNombre.textContent = "";
     }
 
     if (email.value.trim() === "" || !isValidEmail(email.value)) {
-        errorEmail.textContent = "Por favor, ingrese un correo electrónico válido.";
         valid = false;
-    } else {
-        errorEmail.textContent = "";
     }
+
 
     if (mensaje.value.trim() === "") {
-        errorMensaje.textContent = "Por favor, ingrese un mensaje.";
         valid = false;
-    } else {
-        errorMensaje.textContent = "";
     }
 
-    if (!valid) {
-        e.preventDefault();
-    }
-});
+    return valid;
+}
 
 function isValidEmail(email) {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return emailPattern.test(email);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const abrirModalBtn = document.getElementById("abrirModal");
-    
-    abrirModalBtn.addEventListener("click", function() {
-        $('#miModal').modal('show');
-    });
+
+function centerModal() {
+    modal.style.top = "50%";
+    modal.style.left = "50%";
+    modal.style.transform = "translate(-50%, -50%)";
+}
+
+closeModal.addEventListener("click", function () {
+    modal.style.display = "none";
+    window.location.href = "./index.html"; 
 });
